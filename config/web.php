188d83ae784b9d56ac6bdf6607f9010c52dad1ff
wodrow <wodrow451611cv@gmail.com>
2016-09-25 15:37:32 +0800

    add templates

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+    'defaultRoute' => 'index',
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
@@ -60,6 +61,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs'=>['127.0.0.1', '192.168.0.*', 'localhost', '::1'],
     ];
 
     $config['bootstrap'][] = 'gii';
